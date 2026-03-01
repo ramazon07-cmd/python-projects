3855efdb60d87574178f387b3537caaa9b110279
ramazon07-cmd <ustau2453@gmail.com>
2026-03-01 16:25:40 +0500

    i started from scratch

---
+++ checkmypass.py
@@ -0,0 +1,19 @@
+import requests
+import hashlib
+
+
+def request_api_data(query_char):
+    url = f'https://api.pwnedpasswords.com/range/{query_char}'
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail)
+    return response
+
+pwned_api_check('password123')
