ee98a2fd4ddf48c355bcde235beadec2b20763f9
ramazon07-cmd <ustau2453@gmail.com>
2026-03-04 20:33:46 +0500

    crash course is ended

--- tutorial/todos/admin.py
+++ tutorial/todos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Todo, Person
 
 # Register your models here.
+
+admin.site.register(Person)
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'completed', 'owner')
+    list_filter = ('completed', 'owner')
+    search_fields = ('title', 'description')
